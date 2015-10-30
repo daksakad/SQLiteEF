6a3588c7592385b5dcee324e66c1060bf8652a55
jromo <jromo@ia.com.mx>
2015-10-29 19:01:36 -0600

    Adding missing configuration on .config files Creating a context

---
+++ DataLayer/Context/SqliteContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Context
+{
+    public class SqliteContext : DbContext
+    {
+        public SqliteContext(string connString)
+            : base(new SQLiteConnection() { ConnectionString = connString }, true)
+        {
+            Database.SetInitializer<SqliteContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Models.ModelOne> ModelOne { get; set; }
+        public DbSet<Models.ModelTwo> ModelTwo { get; set; }
+    }
+}
--- SqliteEF/Program.cs
+++ SqliteEF/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataLayer.Context;
 
 namespace SqliteEF
 {
     class Program
     {
+        private static SqliteContext _dbSqliteContext;
+
         static void Main(string[] args)
         {
+            String connectionString = "Sqlite Path";
+            InitializeContext(connectionString);
+
+            var examplesList = _dbSqliteContext.ModelOne.ToList();
+        }
+
+        public static bool InitializeContext(string connectionString)
+        {
+            try
+            {
+                SQLiteConnectionStringBuilder sqConn = new SQLiteConnectionStringBuilder();
+                sqConn.DataSource = connectionString;
+                _dbSqliteContext = new SqliteContext(sqConn.ConnectionString);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                throw;
+                return false;
+            }
         }
     }
 }
